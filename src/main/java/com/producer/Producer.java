342f0c8cee5fdb696e6e994f004eb043c95af97d
keshavaero0552 <keshav.aero0552@gmail.com>
2017-10-05 19:41:09 -0500

    javaExample With Kafka

---
+++ src/main/java/com/producer/Producer.java
@@ -0,0 +1,34 @@
+package com.producer;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class Producer {
+
+
+	public static void main(String[] args) {
+		Properties properties=new Properties();
+		properties.put("bootstrap.servers", "localhost:9092");
+		properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		
+		KafkaProducer<String,String> myProducer= new KafkaProducer<String,String>(properties);
+		
+			try {
+			
+			for(int i=1;i<=50;i++){
+				myProducer.send(new  ProducerRecord<String, String>("second-topic","message 1", "Message Value : " + Integer.toString(i)));
+			//	myProducer.send(new  ProducerRecord<String, String>("my-fifth-topic", "url:<local-directory-path>/file"));
+
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			myProducer.close();
+		}
+	}
+
+}
